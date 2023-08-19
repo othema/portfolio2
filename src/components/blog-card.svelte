1c554145aa0f38650e200eb7c878709ee7c02a5a
Daniel George <djv.george@gmail.com>
2023-08-19 20:39:20 +0200

    chore: improve accessibility

--- src/components/blog-card.svelte
+++ src/components/blog-card.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-    import Link from "./link.svelte";
-
   export let title: string;
   export let image: string;
   export let description: string = "";
   export let tags: string[] = [];
 </script>
 
-<div class="group cursor-pointer flex gap-6">
+<a class="group cursor-pointer flex gap-6" href="#">
   <img src={image} class="w-52 aspect-[3/2] object-cover rounded-lg" alt={title} />
-  <div>
+  <article>
     <h2 class="font-medium text-xl group-hover:text-indigo-600 group-hover:underline group-hover:after:content-['_→']">{title}</h2>
     <p class="text-gray-600 text-sm my-3">{description}</p>
     
@@ -18,5 +16,5 @@
         <span class="px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">{tag}</span>
       {/each}
     </div>
-  </div>
-</div>
+  </article>
+</a>
