e82dcd9071973dbe102c509a09f5bdb0955ebf77
Daniel George <djv.george@gmail.com>
2023-08-20 23:45:51 +0200

    feat: add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Link from "../components/link.svelte";
+</script>
+
+<svelte:head>
+  <title>404 | Daniel George</title>
+</svelte:head>
+
+<h1 class="text-center text-3xl font-extrabold tracking-[-0.04em] text-black sm:text-5xl sm:leading-[3.5rem]">
+  Hmm... Somebody's lost.
+</h1>
+<p class="text-center text-gray-500 my-3 mb-6">I'll walk you back <Link href="/" target="">home</Link>.</p>
--- src/routes/blog/[id]/+page.ts
+++ src/routes/blog/[id]/+page.ts
@@ -1,15 +1,18 @@
 import { error } from "@sveltejs/kit";
 
 export async function load({ params }: { params: any }) {
-  const post = await import(`../../../posts/${params.id}.md`)
-  const { default: content, metadata } = post;
-
-  if (metadata.published) {
-    return {
-      content,
-      ...metadata
+  try {
+    const post = await import(`../../../posts/${params.id}.md`);
+    const { default: content, metadata } = post;
+    if (metadata.published) {
+      return {
+        content,
+        ...metadata
+      }
+    } else {
+      return error(404, "Article does not exist.")
     }
-  } else {
-    return error(404, "Article does not exist.")
+  } catch (_) {
+    return error(404, "Article does not exist.");
   }
 }
