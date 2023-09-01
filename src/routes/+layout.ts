486b3032e7ae3012a6e349bc1eacc2a78cb3f360
Daniel George <programmerpremium@gmail.com>
2023-09-01 14:04:10 +0100

    feat: add vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,8 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
+
 export function load({ url }: { url: any }) {
   return {
     url: url.pathname,
